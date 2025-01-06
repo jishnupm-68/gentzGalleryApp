21ad536b7cb7c50e2dbd1bbd2b4ac35e6d299d15
Jishnu PM <jishnupmbcr68@gail.com>
2025-01-06 15:58:35 +0530

    Database

---
+++ models/orderSchema.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+const {Schema} =  mongoose;
+
+const orderSchema = new Schema({
+    orderedItems:[{
+        product:{
+            required:true
+        },
+        quantity:{
+            type:Number,
+            required:true
+        },price:{
+            type:Number,
+            default:0
+        }
+    }],
+    totalPrice:{
+        type:Number,
+        required:true
+    },
+    discount:{
+        type:Number,
+        default:0
+    },
+    finalAmount:{
+        type:Number,
+        required:true
+    },
+    address:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    invoiceDate:{
+        type:Date
+    },
+    status:{
+        type:String,
+        required:true,
+        enum:['Pending', "Processing", "Shipped","Delivered","Cancelled","Return Request","Returned"]
+    },
+    createdOn:{
+        type:Date,
+        default:Date.now,
+        required:true
+    },
+    couponApplied:{
+        type:Boolean,
+        default:false
+    }
+})
+
+
+
+const Order = mongoose.model("Order",orderSchema);
+
+module.exports = Order
